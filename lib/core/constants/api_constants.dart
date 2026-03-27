0a90f10b1e57c16b9f9abaede88d1f67df030e32
Muhammad Tahir <mtahirmusman2@gmail.com>
2026-03-27 17:59:13 +0500

    live app

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,8 +2,8 @@ class ApiConstants {
   // Use http://10.0.2.2:3000/api for Android emulator
   // Use http://localhost:3000/api for iOS simulator
   // Use your computer's IP address (e.g., http://192.168.1.100:3000/api) for physical devices
-  static const String baseUrl = "http://10.0.2.2:3000/api";
-  static const String baseImageUrl = "http://10.0.2.2:3000";
+  static const String baseUrl = "https://api.contact.afaqmis.com/api";
+  static const String baseImageUrl = "https://api.contact.afaqmis.com";
   static const String authLogin = "$baseUrl/auth/login";
   static const String authRegister = "$baseUrl/auth/register";
   static const String authMe = "$baseUrl/auth/me";
